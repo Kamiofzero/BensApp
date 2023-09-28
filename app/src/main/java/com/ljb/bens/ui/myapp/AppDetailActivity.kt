6fc847a45b1bfa85de7c89b0e0a12139362b43e5
lijianbin <974662195@qq.com>
2023-09-28 15:42:33 +0800

    1.0.1

--- app/src/main/java/com/ljb/bens/ui/myapp/AppDetailActivity.kt
+++ app/src/main/java/com/ljb/bens/ui/myapp/AppDetailActivity.kt
@@ -3,8 +3,9 @@ package com.ljb.bens.ui.myapp
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.ljb.base.activity.BaseActivity
-import com.ljb.bens.R
+import com.ljb.base.utils.LogUtil
 import com.ljb.bens.databinding.ActivityAppDetailBinding
+
 
 class AppDetailActivity : BaseActivity<ActivityAppDetailBinding, AppDetailViewModel>() {
 
@@ -13,16 +14,32 @@ class AppDetailActivity : BaseActivity<ActivityAppDetailBinding, AppDetailViewMo
 
     override fun initView() {
 
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(vb.toolbar)
 
         var index = intent.getIntExtra("index", 0)
         var appInfo = MyAppRepository.instance.getApp(index)
 
-        vb.toolbarLayout.title = appInfo.appName
+//        vb.toolbarLayout.title = appInfo.appName
+        vb.toolbarLayout.isTitleEnabled = false
+        supportActionBar?.title = appInfo.appName
+
         vb.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        vb.appBar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            LogUtil.i("verticalOffset: $verticalOffset")
+            supportActionBar?.title =
+                if (Math.abs(verticalOffset) - appBarLayout.totalScrollRange == 0) {
+                    // Collapsed
+                    appInfo.appName
+
+                } else {
+                    // Expanded
+                    ""
+                }
+        };
     }
 
     override fun initData() {
